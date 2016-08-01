682661d76fe8b28ede62f4a36904f2ca9b257a06
Jhonathan <Jhonathanjava@gmail.com>
2016-08-01 00:16:18 -0300

    model Negociacao

--- argentum/src/main/java/br/com/alura/argentum/modelo/Negociacao.java
+++ argentum/src/main/java/br/com/alura/argentum/modelo/Negociacao.java
@@ -1,5 +1,26 @@
 package br.com.alura.argentum.modelo;
 
+import java.time.LocalDateTime;
+
 public class Negociacao {
 
+	private double preco;
+	
+	private int quantidade;
+	
+	private LocalDateTime data;
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+	
+	
 }
